f6c774bbabf68a78d54fa159b003906450dd212a
jenama <johanneenama@gmail.com>
2020-01-07 20:28:21 -0500

    save current work

--- client/src/Components/Search.jsx
+++ client/src/Components/Search.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import SearchItems from './searchItems'
+import axios from 'axios'
 
 class Search extends React.Component {
     constructor (props) {
@@ -20,12 +22,16 @@ class Search extends React.Component {
     }
 
     //This is function is for the form. 
-    handleSubmit = (e) => {
+    handleSubmit = async(e) => {
         e.preventDefault()
-<<<<<<< HEAD
+
         console.log('submit', e)
        
-=======
+
+
+        console.log('submit', e)
+       
+
         const { search, userChecked, circleChecked } = this.state
         let URL;
         if (circleChecked === true) {
@@ -40,23 +46,24 @@ class Search extends React.Component {
             console.log(err)
             console.log('not found')
         }
->>>>>>> 142978dc781b300c8b708e4616dba1ab32fe1efb
+
     }
 
     render() {
+        
         return (
             <div className='search'>
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Search
-<<<<<<< HEAD
-                        <input type='text' onChange={this.handleSearchBar} value={this.state.searchBar}></input>
 
-=======
+                        <input type='text' onChange={this.handleSearchBar} value={this.state.searchBar}></input>
                         <input type='text' onChange={this.handleSearchChange} value={search}></input>
->>>>>>> 142978dc781b300c8b708e4616dba1ab32fe1efb
+
+
                     </label>
-                    <input type='submit' value='submit'></input>
+
+                        <input type='submit' value='submit'></input>
                 </form>
             </div>
         )
