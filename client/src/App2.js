d4d9d2e142d7e5bf516bd4255eee23616fc3b76f
Hupaul <hupaulcamacho@pursuit.org>
2020-01-11 17:44:24 -0500

    edit app2js

--- client/src/App2.js
+++ client/src/App2.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import './App2.css';
 import Mainpage from './Components/Mainpage'
 import Login from './Login'
@@ -14,11 +14,11 @@ const App2 = () => (
         {"  "}
         <Link to="/signup">Sign Up</Link>
       </nav>
-      <div>
+      <Switch>
         <Route exact path="/" component={Mainpage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
-      </div>
+      </Switch>
     </div>
   );
 
