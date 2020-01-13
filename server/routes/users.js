18f98d4d9cf590c0370e6b9ba17fa72d6beb6e50
jenama <johanneenama@gmail.com>
2020-01-13 15:04:31 -0500

    fixed the patch route for users & removed the owner_avi from database.

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,66 +20,66 @@ const upload = multer({
 ///////////////////////////////
 
 //Route to get all the users 
-router.get('/', async(req, res) => {
-   
+router.get('/', async (req, res) => {
+
   try {
     const requestQuery = `SELECT * FROM users`
-   
-    let allUsers = await db.any(requestQuery)
-       console.log('users', allUsers)
-      res.json({
-        data: allUsers,
-        message: `All users were successfully retieved`
-      })
 
-  } catch (err){
+    let allUsers = await db.any(requestQuery)
+    console.log('users', allUsers)
+    res.json({
+      data: allUsers,
+      message: `All users were successfully retieved`
+    })
+
+  } catch (err) {
     res.status(404)
     res.json({
       message: `The request has failed`
     })
-    
+
   }
 })
 /////////////////////////////////////
 
 //Router to get users by username
-router.get('/username/:username', async(req, res) => {
-    let userInfo = req.params.username
+router.get('/username/:use', async (req, res) => {
+  let userInfo = req.params.username
  
 
-    console.log('params!!!!1', userInfo)
-    let requestQuery = ``
+  console.log('params!!!!1', userInfo)
+  let requestQuery = ``
   if (by === 'email') {
-     requestQuery = `SELECT * FROM users WHERE email LIKE $1`
+    requestQuery = `SELECT * FROM users WHERE email LIKE $1`
   } else {
-     requestQuery = `SELECT * FROM users WHERE username LIKE $1`
+    requestQuery = `SELECT * FROM users WHERE username LIKE $1`
   }
- 
-    try {
-    
-      let user = await db.one(requestQuery, [`%${userInfo}%`])
-         console.log('users email!!!!!', user)
 
-      res.json({
-        data: user,
-        message: `The users were successfully retrieved`
-      })
-    } catch (err) {
-      console.log('error', err)
-      res.status(404)
-      res.json({
-        message: `Failure to retrieve users`
-      })
-    }
+  try {
+
+    let user = await db.one(requestQuery, [`%${userInfo}%`])
+    console.log('users email!!!!!', user)
+
+    res.json({
+      data: user,
+      message: `The users were successfully retrieved`
+    })
+  } catch (err) {
+    console.log('error', err)
+    res.status(404)
+    res.json({
+      message: `Failure to retrieve users`
+    })
+  }
 })
 
 // User Authentication
 router.get('/login/:username/:password', async (req, res) => {
   let username = req.params.username
-  let password = req.params.password  
+  let password = req.params.password
   let loginQuery = `SELECT * FROM users WHERE username = $1 AND password = $2`
 
-  try  {
+  try {
     let user = await db.any(loginQuery, [username, password])
     res.json({
       message: 'login sucessful',
@@ -132,41 +132,69 @@ router.post('/', upload.single('avatar'), async (req, res) => {
 ///////////////////////////////////
 
 //Route to upadate the user's information
-router.patch('/:id', async(req, res) => {
+router.patch('/:id', async (req, res) => {
 
-  let userId  = req.params.id
+  let by = req.params.by
+  let value = req.params.value
+  let query = `UPDATE users SET `
+
+  // let updateQuery = ``
+  if (req.body.email) {
+    query += `email = $/email/, `
+  }
+  if (req.body.username) {
+    query += `username= $/username/, `
+  }
+  if (req.body.avatar) {
+    query += `avatar = $/avatar/, `
+  }
+  if (req.body.password) {
+    query += `password = $/password/, ` 
+  }
+  query = query.slice(0, query.length -2)
+   query+= ` WHERE id = $/id/`
+  console.log('query', query)
+
   try {
-    const updateQuery = `UPDATE USERS SET username=$1, email=$2, avatar=$3 WHERE id=22`;
-    await db.any(updateQuery, [req.body.username, req.body.email, req.body.avatar], userId)
-    
+
+    await db.any(query, {
+        email:req.body.email, 
+        username:req.body.username,
+        avatar: req.body.avatar,
+        password: req.body.password,
+        id: req.params.id
+      }
+    )
+
     let data = {
       username: req.body.username,
       email: req.body.email,
-      avatar: req.body.avatar
+      avatar: req.body.avatar,
+      password: req.body.password
     }
 
-     res.json({
+    res.json({
       user: data,
       message: `The user's information has been successfully updated`
     })
-  } catch (err){
+  } catch (err) {
     res.status(404)
-     res.json({
-       message: `Something went wrong!`
-     })
- }
+    res.json({
+      message: `Something went wrong!`
+    })
+  }
 })
 ///////////////////////////////////////
 
 //Route to delete a user
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
   let userId = req.params.id
   try {
     const deleteQuery = `DELETE FROM users WHERE id = $1`
-    await db.none(deleteQuery, [userId] )
+    await db.none(deleteQuery, [userId])
 
     let data = {
-       username: req.body.username,
+      username: req.body.username,
       email: req.body.email,
       avatar: req.body.avatar
     }
@@ -175,7 +203,7 @@ router.delete('/:id', async(req, res) => {
       message: `User was successfully deleted`
     })
 
-  }catch (error) {
+  } catch (error) {
     res.status(404)
     res.json({
       message: `User not deleted!`
