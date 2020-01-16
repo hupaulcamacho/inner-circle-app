7ff4dfe84aea85153263371aa05bac18f5ec054f
jenama <johanneenama@gmail.com>
2020-01-15 21:55:34 -0500

    save

--- server/routes/users.js
+++ server/routes/users.js
@@ -71,12 +71,6 @@ router.post('/login/:username/:password', async (req, res) => {
   UPDATE users SET loggedIn = true WHERE username = $1;
   SELECT * FROM users WHERE username = $1 AND password = $2;
   `
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 0ac5eba44fa7c095c4412a75c83f6cab594c66b9
   try {
     let user = await db.any(loginQuery, [username, password])
     console.log(user)
