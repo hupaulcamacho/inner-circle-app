47efe3d9c706a440830118dddff1348f5e4e64a9
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-02-01 14:37:56 -0500

    think i fixed what needed fixing

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
 
 const circleRouter = require('./routes/circles');
