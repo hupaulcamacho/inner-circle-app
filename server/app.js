6f85b061f1943e77e027378b27008cce7596916b
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-02-01 14:22:06 -0500

    changed a few things in heroku

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
 
 const circleRouter = require('./routes/circles');
@@ -32,6 +32,10 @@ app.use('/circles', circleRouter);
 
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 
 module.exports = app;
